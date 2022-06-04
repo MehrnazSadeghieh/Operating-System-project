bf0d3ef052fa6d46d22ec7eb3e5c274b8d1443d0
MehrnazSadeghieh <mehrnaz271380@gmail.com>
2022-06-05 00:53:28 +0430

    os-pr2.c

--- os-pr2.c
+++ os-pr2.c
@@ -19,31 +19,31 @@ void *thread1Function(void* arg){
     FILE * file;
     file = fopen("wordlist.txt","r");
     while(fgets(word, sizeof(word), file)){
-    	while(i<strlen(thread1part)){
-    		if(thread1part[i] == ' ' || thread1part[i] == '\0' || thread1part[i] == '\0')
-    			break;
-    		////copy word to third array
-    		arr[j++] = thread1part[i++];
-    	}
-    	arr[j] = '\0';
-    	j = 0;
-    	/////if word mached, flag turns true
-    	if(strcmp(word,arr) == 0)
-    		flag = 1;
-    	i++;
-    	////write in file
-    	if(flag == 1){
-    		// time_t t;
-    		// time(&t);
-    		// fprintf(file1, "%s : \n", word);
-    		// fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
-    		// fprintf(file1, "thread 1 found this word\n");
-    		// time(&t);
-    		// fprintf(file1, "time of writing in file %s\n",ctime(&t) );
-    		printf("%s found withh thread1\n", word);
+        while(i<strlen(thread1part)){
+            if(thread1part[i] == ' ' || thread1part[i] == '\0' || thread1part[i] == '\0')
+                break;
+            ////copy word to third array
+            arr[j++] = thread1part[i++];
+        }
+        arr[j] = '\0';
+        j = 0;
+        /////if word mached, flag turns true
+        if(strcmp(word,arr) == 0)
+            flag = 1;
+        i++;
+        ////write in file
+        if(flag == 1){
+            time_t t;
+            time(&t);
+            fprintf(file1, "%s : \n", word);
+            fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
+            fprintf(file1, "thread 1 found this word\n");
+            time(&t);
+            fprintf(file1, "time of writing in file %s\n",ctime(&t) );
+            printf("%s found withh thread1\n", word);
 
-    	}
-    	flag = -1;
+        }
+        flag = -1;
     }
     pthread_mutex_unlock(&lock);
     return 0;
@@ -57,31 +57,31 @@ void *thread2Function(void* arg){
     FILE * file;
     file = fopen("wordlist.txt","r");
         while(fgets(word, sizeof(word), file)){
-    	while(i<strlen(thread2part)){
-    		if(thread2part[i] == ' ' || thread2part[i] == '\0' || thread2part[i] == '\0')
-    			break;
-    		////copy word to third array
-    		arr[j++] = thread2part[i++];
-    	}
-    	arr[j] = '\0';
-    	j = 0;
-    	/////if word mached, flag turns true
-    	if(strcmp(word,arr) == 0)
-    		flag = 1;
-    	i++;
-    	////write in file
-    	if(flag == 1){
-    		// time_t t;
-    		// time(&t);
-    		// fprintf(file1, "%s : \n", word);
-    		// fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
-    		// fprintf(file1, "thread 2 found this word\n");
-    		// time(&t);
-    		// fprintf(file1, "time of writing in file %s\n",ctime(&t) );
-    		printf("%s found withh thread1\n", word);
+        while(i<strlen(thread2part)){
+            if(thread2part[i] == ' ' || thread2part[i] == '\0' || thread2part[i] == '\0')
+                break;
+            ////copy word to third array
+            arr[j++] = thread2part[i++];
+        }
+        arr[j] = '\0';
+        j = 0;
+        /////if word mached, flag turns true
+        if(strcmp(word,arr) == 0)
+            flag = 1;
+        i++;
+        ////write in file
+        if(flag == 1){
+            // time_t t;
+            // time(&t);
+            // fprintf(file1, "%s : \n", word);
+            // fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
+            // fprintf(file1, "thread 2 found this word\n");
+            // time(&t);
+            // fprintf(file1, "time of writing in file %s\n",ctime(&t) );
+            printf("%s found withh thread1\n", word);
 
-    	}
-    	flag = -1;
+        }
+        flag = -1;
     }
     pthread_mutex_unlock(&lock);
     return 0;
@@ -96,31 +96,31 @@ void *thread3Function(void* arg){
     file = fopen("wordlist.txt","r");
 
     while(fgets(word, sizeof(word), file)){
-    	while(i<strlen(thread3part)){
-    		if(thread3part[i] == ' ' || thread3part[i] == '\0' || thread3part[i] == '\0')
-    			break;
-    		////copy word to third array
-    		arr[j++] = thread3part[i++];
-    	}
-    	arr[j] = '\0';
-    	j = 0;
-    	/////if word mached, flag turns true
-    	if(strcmp(word,arr) == 0)
-    		flag = 1;
-    	i++;
-    	////write in file
-    	if(flag == 1){
-    		// time_t t;
-    		// time(&t);
-    		// fprintf(file1, "%s : \n", word);
-    		// fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
-    		// fprintf(file1, "thread 3 found this word\n");
-    		// time(&t);
-    		// fprintf(file1, "time of writing in file %s\n",ctime(&t) );
-    		printf("%s found withh thread1\n", word);
+        while(i<strlen(thread3part)){
+            if(thread3part[i] == ' ' || thread3part[i] == '\0' || thread3part[i] == '\0')
+                break;
+            ////copy word to third array
+            arr[j++] = thread3part[i++];
+        }
+        arr[j] = '\0';
+        j = 0;
+        /////if word mached, flag turns true
+        if(strcmp(word,arr) == 0)
+            flag = 1;
+        i++;
+        ////write in file
+        if(flag == 1){
+            // time_t t;
+            // time(&t);
+            // fprintf(file1, "%s : \n", word);
+            // fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
+            // fprintf(file1, "thread 3 found this word\n");
+            // time(&t);
+            // fprintf(file1, "time of writing in file %s\n",ctime(&t) );
+            printf("%s found withh thread1\n", word);
 
-    	}
-    	flag = -1;
+        }
+        flag = -1;
     }
     pthread_mutex_unlock(&lock);
     return 0;
@@ -135,31 +135,31 @@ void *thread4Function(void* arg){
     file = fopen("wordlist.txt","r");
 
     while(fgets(word, sizeof(word), file)){
-    	while(i<strlen(thread1part)){
-    		if(thread4part[i] == ' ' || thread4part[i] == '\0' || thread4part[i] == '\0')
-    			break;
-    		////copy word to third array
-    		arr[j++] = thread4part[i++];
-    	}
-    	arr[j] = '\0';
-    	j = 0;
-    	/////if word mached, flag turns true
-    	if(strcmp(word,arr) == 0)
-    		flag = 1;
-    	i++;
-    	////write in file
-    	if(flag == 1){
-    		// time_t t;
-    		// time(&t);
-    		// fprintf(file1, "%s : \n", word);
-    		// fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
-    		// fprintf(file1, "thread 4 found this word\n");
-    		// time(&t);
-    		// fprintf(file1, "time of writing in file %s\n",ctime(&t) );
-    		printf("%s found withh thread1\n", word);
+        while(i<strlen(thread1part)){
+            if(thread4part[i] == ' ' || thread4part[i] == '\0' || thread4part[i] == '\0')
+                break;
+            ////copy word to third array
+            arr[j++] = thread4part[i++];
+        }
+        arr[j] = '\0';
+        j = 0;
+        /////if word mached, flag turns true
+        if(strcmp(word,arr) == 0)
+            flag = 1;
+        i++;
+        ////write in file
+        if(flag == 1){
+            // time_t t;
+            // time(&t);
+            // fprintf(file1, "%s : \n", word);
+            // fprintf(file1,"this word hase been found at : %s\n", ctime(&t));
+            // fprintf(file1, "thread 4 found this word\n");
+            // time(&t);
+            // fprintf(file1, "time of writing in file %s\n",ctime(&t) );
+            printf("%s found withh thread1\n", word);
 
-    	}
-    	flag = -1;
+        }
+        flag = -1;
     }
     pthread_mutex_unlock(&lock);
     return 0;
@@ -167,84 +167,84 @@ void *thread4Function(void* arg){
 
 
 int wordCount(){
-	FILE * file;
-	int count = 0;
-	file = fopen("text.txt","r");
-	char ch;
-	while((ch = fgetc(file)) !=  EOF){
-		if (ch == ' ' || ch == '\n')
+    FILE * file;
+    int count = 0;
+    file = fopen("text.txt","r");
+    char ch;
+    while((ch = fgetc(file)) !=  EOF){
+        if (ch == ' ' || ch == '\n')
             count++;
-	}
-	fclose(file);
-	return count;
+    }
+    fclose(file);
+    return count;
 }
 
 void threadpartstring(char arr1[], char arr2[], char arr3[], char arr4[], int count1, int count2, int count3, int count4){
-	FILE * file;
-	file = fopen("text.txt","r");
-	int n = 0;
-	char ch;
-	int i = 0, j = 0;
-	////thread1part
-	while(i < count1){
-		ch = fgetc(file);
-		arr1[j] = ch;
-		if(ch == ' ' || ch == '\n')
-			i++;
-		j++;
-	}
-	////thread2part
-	i = 0;
-	j = 0;
-	while(i < count2){
-		ch = fgetc(file);
-		arr2[j] = ch;
-		if(ch == ' ' || ch == '\n')
-			i++;
-		j++;
-	}
-	////thread3part
-	i = 0;
-	j = 0;
-	while(i < count3){
-		ch = fgetc(file);
-		arr3[j] = ch;
-		if(ch == ' ' || ch == '\n')
-			i++;
-		j++;
-	}
-	////thread4part
-	i = 0;
-	j = 0;
-	while(i < count4){
-		ch = fgetc(file);
-		arr4[j] = ch;
-		if(ch == ' ' || ch == '\n')
-			i++;
-		j++;
-	}
-	fclose(file);
+    FILE * file;
+    file = fopen("text.txt","r");
+    int n = 0;
+    char ch;
+    int i = 0, j = 0;
+    ////thread1part
+    while(i < count1){
+        ch = fgetc(file);
+        arr1[j] = ch;
+        if(ch == ' ' || ch == '\n')
+            i++;
+        j++;
+    }
+    ////thread2part
+    i = 0;
+    j = 0;
+    while(i < count2){
+        ch = fgetc(file);
+        arr2[j] = ch;
+        if(ch == ' ' || ch == '\n')
+            i++;
+        j++;
+    }
+    ////thread3part
+    i = 0;
+    j = 0;
+    while(i < count3){
+        ch = fgetc(file);
+        arr3[j] = ch;
+        if(ch == ' ' || ch == '\n')
+            i++;
+        j++;
+    }
+    ////thread4part
+    i = 0;
+    j = 0;
+    while(i < count4){
+        ch = fgetc(file);
+        arr4[j] = ch;
+        if(ch == ' ' || ch == '\n')
+            i++;
+        j++;
+    }
+    fclose(file);
 }
 
 
 
 int main(){
 
-	int count;
-	count = wordCount();
-	int t1WCount,t2WCount,t3WCount,t4WCount;
-	pthread_t tid1,tid2,tid3;
+    int count;
+    count = wordCount();
+    int t1WCount,t2WCount,t3WCount,t4WCount;
+    pthread_t tid1,tid2,tid3;
 
-	t1WCount = count/4;
-	t2WCount = count/4;
-	t3WCount = count/4;
-	t4WCount = count - (3*count/4);
+    t1WCount = count/4;
+    t2WCount = count/4;
+    t3WCount = count/4;
+    t4WCount = count - (3*count/4);
 
-	threadpartstring(thread1part,thread2part,thread3part,thread4part,t1WCount,t2WCount,t3WCount,t4WCount);
-	printf("%s\n", thread1part);
-	printf("%s\n", thread2part);
-	printf("%s\n", thread3part);
-	printf("%s\n", thread4part);
+    threadpartstring(thread1part,thread2part,thread3part,thread4part,t1WCount,t2WCount,t3WCount,t4WCount);
+    printf("%s\n", thread1part);
+    printf("%s\n", thread2part);
+    printf("%s\n", thread3part);
+    printf("%s\n", thread4part);
 
     ///file1 = fopen("out.txt","w");
 
@@ -256,5 +256,5 @@ int main(){
     pthread_join(tid2,NULL);
     pthread_join(tid3,NULL);
 
-	return 0;
+    return 0;
 }
